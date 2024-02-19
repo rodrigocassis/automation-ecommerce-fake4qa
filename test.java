52acb1211e80c39b00703e155aba56ca30f67da1
Rodrigo Cassis <71719122+rodrigocassis@users.noreply.github.com>
2024-02-19 17:40:01 -0300

    Update test.java

--- test.java
+++ test.java
@@ -1,20 +1,10 @@
-<dependency>
-    <groupId>org.bouncycastle</groupId>
-    <artifactId>bcprov-jdk15on</artifactId>
-    <version>1.68</version>
-</dependency>
-
-
-
-    import io.restassured.RestAssured;
+import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import io.restassured.response.Response;
 
 import java.io.File;
-import java.security.KeyStore;
-import java.security.Security;
 
-public class ExemploRestAssuredComCertificado {
+public class ExemploChamadaAPIComCertificadoEProxy {
 
     public static void main(String[] args) {
         // Caminho para o arquivo cert.crt
@@ -23,31 +13,36 @@ public class ExemploRestAssuredComCertificado {
         // Caminho para o arquivo chave.key
         String chavePath = "caminho/do/seu/arquivo/chave.key";
 
-        // Adiciona o provedor Bouncy Castle
-        Security.addProvider(new BouncyCastleProvider());
+        // URL da API
+        String apiUrl = "https://token.com.br/token/";
 
-        // Faz a chamada usando o certificado e a chave privada
+        // Configuração do proxy (substitua pelos seus valores)
+        String proxyHost = "seu_proxy_host";
+        int proxyPort = 8080;
+
+        // Configuração do RestAssured
+        RestAssured.baseURI = apiUrl;
+        RestAssured.useRelaxedHTTPSValidation(); // Use se o certificado não for validado por uma CA confiável
+
+        // Configuração do certificado e chave privada
         RequestSpecification request = RestAssured.given().relaxedHTTPSValidation()
-                .config(RestAssured.config().sslConfig().keyStore(loadKeyStore(chavePath, "")).and().trustStore(certificadoPath));
+                .config(RestAssured.config()
+                        .sslConfig()
+                        .keyStore(chavePath, "")
+                        .and()
+                        .trustStore(certificadoPath))
+                .proxy(proxyHost, proxyPort); // Configuração do proxy
 
-        // Exemplo de chamada (substitua pelo seu código real)
-        // request.get("/seu-endpoint").then().statusCode(200);
-    }
+        // Parâmetros do formulário
+        String parametroId = "3423423455";
 
-    private static KeyStore loadKeyStore(String chavePath, String senha) {
-        try {
-            KeyStore keyStore = KeyStore.getInstance("PKCS12", "BC");
-            keyStore.load(null, null);
+        // Faz a chamada POST
+        Response response = request
+                .formParam("meu_id", parametroId)
+                .post();
 
-            // Adiciona a chave privada ao KeyStore
-            keyStore.setKeyEntry("alias", // escolha um alias
-                    KeyUtils.loadPrivateKey(chavePath, ""), // senha da chave privada
-                    new char[0], // senha da chave privada (char[])
-                    KeyUtils.loadCertificateChain(chavePath)); // certificado associado à chave privada
-
-            return keyStore;
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao carregar o KeyStore", e);
-        }
+        // Exibe o status code e a resposta
+        System.out.println("Status Code: " + response.getStatusCode());
+        System.out.println("Resposta: " + response.getBody().asString());
     }
 }
