49e24a68cbec65543a69214d65305b0ef9b0c92a
Rodrigo Cassis <71719122+rodrigocassis@users.noreply.github.com>
2024-02-19 17:52:55 -0300

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,10 @@ import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 public class ExemploChamadaAPIComCertificadoEProxy {
 
@@ -23,27 +27,40 @@ public class ExemploChamadaAPIComCertificadoEProxy {
         String proxyUser = "seu_usuario";
         String proxyPassword = "sua_senha";
 
-        // Configuração do RestAssured
-        RestAssured.baseURI = apiUrl;
-        RestAssured.useRelaxedHTTPSValidation(); // Use se o certificado não for validado por uma CA confiável
+        try {
+            // Configuração do RestAssured
+            RestAssured.baseURI = apiUrl;
+            RestAssured.useRelaxedHTTPSValidation(); // Use se o certificado não for validado por uma CA confiável
 
-        // Faz a chamada usando o certificado e a chave privada
-        RequestSpecification request = RestAssured.given().relaxedHTTPSValidation()
-                .keyStore(chavePath, "")
-                .and()
-                .trustStore(certificadoPath)
-                .proxy(proxyHost, proxyPort) // Configuração do proxy
-                .proxyUser(proxyUser) // Configuração do usuário do proxy
-                .proxyPassword(proxyPassword); // Configuração da senha do proxy
+            // Carregamento do certificado como X509Certificate
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(new FileInputStream(certificadoPath));
 
-        // Parâmetros do formulário
-        String parametroId = "3423423455";
+            // Configuração do KeyStore para o certificado
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+            trustStore.setCertificateEntry("alias", certificate);
 
-        // Faz a chamada POST
-        request
-            .formParam("meu_id", parametroId)
-            .post()
-            .then()
-            .statusCode(200);
+            // Faz a chamada usando o certificado, a chave privada e o proxy
+            RequestSpecification request = RestAssured.given().relaxedHTTPSValidation()
+                    .keyStore(chavePath, "")
+                    .and()
+                    .trustStore(trustStore)
+                    .proxy(proxyHost, proxyPort)
+                    .proxyUser(proxyUser)
+                    .proxyPassword(proxyPassword);
+
+            // Parâmetros do formulário
+            String parametroId = "3423423455";
+
+            // Faz a chamada POST
+            request
+                .formParam("meu_id", parametroId)
+                .post()
+                .then()
+                .statusCode(200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
