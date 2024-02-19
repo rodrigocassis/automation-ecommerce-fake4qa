95e2a42958b1fe32ceea8cebe8177b2757e73b0b
Rodrigo Cassis <71719122+rodrigocassis@users.noreply.github.com>
2024-02-19 15:32:29 -0300

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 
-import javax.net.ssl.KeyManagerFactory;
-import java.io.FileInputStream;
-import java.security.KeyStore;
+import java.io.File;
 
 public class ExemploRestAssuredComCertificado {
 
@@ -18,26 +16,13 @@ public class ExemploRestAssuredComCertificado {
         RestAssured.baseURI = "sua_base_uri";
         RestAssured.useRelaxedHTTPSValidation();
 
-        try {
-            // Carrega o certificado
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(new FileInputStream(certificadoPath), null);
+        // Configuração do certificado e chave privada
+        RestAssured.config = RestAssured.config().sslConfig(
+                RestAssured.config().sslConfig().keyStore(chavePath, "")
+        );
 
-            // Configuração do KeyManager
-            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            keyManagerFactory.init(keyStore, null);
-
-            // Configuração do SSLConfig
-            io.restassured.config.SSLConfig sslConfig = new io.restassured.config.SSLConfig().with().keyStore(keyStore);
-
-            // Configura o RestAssured para usar o certificado e chave privada
-            RestAssured.config = RestAssured.config().sslConfig(sslConfig).sslConfig().keyManager(keyManagerFactory);
-
-            // Faz a chamada usando o certificado configurado
-            RequestSpecification request = RestAssured.given();
-            // ... seu código de requisição continua aqui
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Faz a chamada usando o certificado configurado
+        RequestSpecification request = RestAssured.given();
+        // ... seu código de requisição continua aqui
     }
 }
