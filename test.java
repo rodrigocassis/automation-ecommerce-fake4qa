8f1e20bdf6373493d7ce8243d97da8d0354ae648
Rodrigo Cassis <71719122+rodrigocassis@users.noreply.github.com>
2024-02-19 16:59:43 -0300

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,5 @@
 import io.restassured.RestAssured;
-import io.restassured.config.SSLConfig;
 import io.restassured.specification.RequestSpecification;
-
-import java.io.FileInputStream;
-import java.security.KeyStore;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
 
 public class ExemploRestAssuredComCertificado {
 
@@ -16,39 +10,11 @@ public class ExemploRestAssuredComCertificado {
         // Caminho para o arquivo chave.key
         String chavePath = "caminho/do/seu/arquivo/chave.key";
 
-        // Configuração do RestAssured
-        RestAssured.baseURI = "https://sua-api.com";
-        RestAssured.useRelaxedHTTPSValidation();
+        // Faz a chamada usando o certificado e a chave privada
+        RequestSpecification request = RestAssured.given().relaxedHTTPSValidation()
+                .config(RestAssured.config().sslConfig().keyStore(chavePath, "").and().trustStore(certificadoPath));
 
-        try {
-            // Carregamento do certificado como X509Certificate
-            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-            X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(new FileInputStream(certificadoPath));
-
-            // Configuração do KeyStore para o certificado
-            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            trustStore.load(null, null);
-            trustStore.setCertificateEntry("sua_alias", certificate);
-
-            // Configuração do certificado e chave privada
-            SSLConfig sslConfig = new SSLConfig()
-                    .with()
-                    .keyStore(chavePath, "")
-                    .and()
-                    .trustStore(trustStore);
-
-            // Configura o RestAssured para usar o certificado e chave privada
-            RestAssured.config = RestAssured.config().sslConfig(sslConfig);
-
-            // Faz a chamada usando o certificado configurado
-            RequestSpecification request = RestAssured.given();
-
-            // Seu código de requisição continua aqui
-            // Exemplo: request.get("/seu-endpoint").then().statusCode(200);
-
-            // Lembre-se de ajustar o endpoint conforme necessário
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Exemplo de chamada (substitua pelo seu código real)
+        // request.get("/seu-endpoint").then().statusCode(200);
     }
 }
